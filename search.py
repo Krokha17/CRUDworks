b1c0fab586df12b124da1c69c09ac683b6a86e14
Маринка <105660089+Krokha17@users.noreply.github.com>
2022-12-01 10:55:45 +0300

    Delete search.py

--- search.py
+++
@@ -1,9 +0,0 @@
-def SearchBracket(s):
-    bracket = False
-    i = 0
-    while i < int (len (s) ):
-        if s[i] == '(' or s[i] == ')':
-            bracket = True
-            break
-        i += 1
-    return bracket
